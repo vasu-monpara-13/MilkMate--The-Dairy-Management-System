105f3a9c3b079cbd6beb1d1a3f1893c9ca300698
vasu-monpara-13 <vasumonpara4351@gmail.com>
2026-04-22 16:21:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/MilkMate/",
+  base: "/",
   build: {
     outDir: "dist",
     sourcemap: false,
